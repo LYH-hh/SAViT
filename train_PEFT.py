3212b4eb8ad4d2262c2776ab896fea71ab717493
Lyh <74284798+LYH-hh@users.noreply.github.com>
2025-09-25 11:13:17 +0800

    Update train_PEFT.py

--- train_PEFT.py
+++ train_PEFT.py
@@ -111,7 +111,7 @@ def get_args_parser():
                         help='Use class token instead of global pool for classification')
 
     # Dataset parameters
-    parser.add_argument('--data_path', default='/lyh/dataset/wellPrepared/chest-ray', type=str,
+    parser.add_argument('--data_path', default='', type=str,
                         help='dataset path')
     parser.add_argument('--nb_classes', default=2, type=int,
                         help='number of the classification types')
